43a359db7c7d22962cee16965c4cf761ae48ccba
manvisoftwares <manvisoftwares@gmail.com>
2025-02-22 02:56:32 -0500

    attendance update

--- emp_attendance_submit.php
+++ emp_attendance_submit.php
@@ -13,42 +13,6 @@ if ($numrows > 0) {
     $res = mysqli_query($connection, $out_time);
 } else {
     $query = "INSERT INTO  emp_attendance ( date,in_time,emp_id ) VALUES ('$date', '$time', '$emp_id')";
-    $res = mysqli_query($connection, $query);<?php
-include("config.php");
-
-$date = $_POST['date'];
-$time = $_POST['time'];
-$emp_id = $_POST['emp_id'];
-
-$select = "SELECT * FROM emp_attendance WHERE emp_id = '$emp_id' AND date = CURDATE() ";
-$res = mysqli_query($connection, $select);
-$numrows = mysqli_num_rows($res);
-if ($numrows > 0) {
-    $out_time = "UPDATE  emp_attendance SET out_time ='$time' WHERE emp_id ='$emp_id' AND date = CURDATE()";
-    $res = mysqli_query($connection, $out_time);
-} else {
-    $query = "INSERT INTO  emp_attendance ( date,in_time,emp_id ) VALUES ('$date', '$time', '$emp_id')";
-    $res = mysqli_query($connection, $query);
-}
-
-
-}
-
-<?php
-include("config.php");
-
-$date = $_POST['date'];
-$time = $_POST['time'];
-$emp_id = $_POST['emp_id'];
-
-$select = "SELECT * FROM emp_attendance WHERE emp_id = '$emp_id' AND date = CURDATE() ";
-$res = mysqli_query($connection, $select);
-$numrows = mysqli_num_rows($res);
-if ($numrows > 0) {
-    $out_time = "UPDATE  emp_attendance SET out_time ='$time' WHERE emp_id ='$emp_id' AND date = CURDATE()";
-    $res = mysqli_query($connection, $out_time);
-} else {
-    $query = "INSERT INTO  emp_attendance ( date,in_time,emp_id ) VALUES ('$date', '$time', '$emp_id')";
     $res = mysqli_query($connection, $query);
 }
 
